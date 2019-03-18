82cdfdbf2e004b397b7cc5c8b5f7d909594170f4
Julien Mauvais <julienmauvais@hotmail.fr>
2019-03-18 14:16:48 +0100

    image produit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :produits
-  root 'students#index'
+  root 'produits#index'
   resources :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
